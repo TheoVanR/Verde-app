4c9d048fe63dd6839791f4b3671aac24cc5facfd
Theodor van Reisen <98526113+TheoVanR@users.noreply.github.com>
2024-01-26 18:36:18 +0100

    edit1

---
+++ Verde/src/assets/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <section className="hero">
+                <h1>verde</h1>
+                <p> Authentic mexican tacos</p>
+
+            </section>
+        </div>
+    );
+};
+
+export default Home;
